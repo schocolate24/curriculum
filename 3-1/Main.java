0c8f322942226254857eb197f56df099d2e3298c
schocolate24 <snoopy.peco@gmail.com>
2022-01-01 22:11:23 +0900

    Update Main.java

--- 3-1/Main.java
+++ 3-1/Main.java
@@ -9,9 +9,7 @@ public class Main{
         * 問1
         * int型の配列dataを作成し、値を3,1,2,7,5で初期化しなさい
         */
-
         int[] data = {3,1,2,7,5};
-
         /*
         * 問2
         * 以下のfor文を完成させなさい
@@ -20,21 +18,23 @@ public class Main{
             System.out.print(data[i] + " ");
         }
         System.out.println();
-        for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j > i; j--) {
+         for (int i = 0; i < data.length; i++){
+              for (int j = data.length; j > i; j--){
                 /*
                 * 問3
                 * 以下、配列の添字を入れてソートを完成させなさい
-                */
-                if(data[i] > data[j]){
-                  int box = data[i];
-                  data[i] = data[j];
-                  data[j] = box;
+                */ 
+                if(data[j-1] > data[j]){
+                  int box = data[j];
+                  data[j] = data[j-1];
+                  data[j-1] = box;
                 }
             }
         }
+    }
         for(int i = 0; i < data.length; i++){
             System.out.print(data[i] + " ");
         }
-    }
+}
+}
 }
